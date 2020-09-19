898818ebb37eb492e40300f60fdae0bb8d251154
7777slon <sounddaylight@gmail.com>
2020-09-19 20:31:42 +0300

    UI improves; Dependencies added;

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.news;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefresherLayout;
+    private NestedScrollView nestedScrollView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setNestedScrollingEnabled(false);
+        swipeRefresherLayout = findViewById(R.id.swipeRefresherLayout);
+        swipeRefresherLayout.setColorSchemeResources(R.color.colorAccent);
+        nestedScrollView = findViewById(R.id.nestedScrollView);
     }
 }
