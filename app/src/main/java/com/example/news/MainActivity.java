dd8e137174e32b9ca962ef236462debdf3fed326
7777slon <sounddaylight@gmail.com>
2020-09-19 21:28:43 +0300

    Main menu implemented;

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.news;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +31,40 @@ public class MainActivity extends AppCompatActivity {
         swipeRefresherLayout = findViewById(R.id.swipeRefresherLayout);
         swipeRefresherLayout.setColorSchemeResources(R.color.colorAccent);
         nestedScrollView = findViewById(R.id.nestedScrollView);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        assert (searchManager != null);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setQueryHint("Search Latest News...");
+        nestedScrollView.setOnTouchListener((v, event) -> {
+            searchView.clearFocus();
+            final Activity activity = MainActivity.this;
+            final View view = activity.getWindow().getDecorView();
+            final InputMethodManager imm = (InputMethodManager) activity
+                    .getSystemService(Context.INPUT_METHOD_SERVICE);
+            assert (imm != null);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            return false;
+        });
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.clearFocus();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return true;
     }
 }
