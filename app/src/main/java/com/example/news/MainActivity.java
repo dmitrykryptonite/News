bb708549dd7d4b30f0b71b52f136654b8c49f742
7777slon <sounddaylight@gmail.com>
2020-09-20 13:53:41 +0300

    UI improves;

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.news;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvActualNews;
+    private SwipeRefreshLayout swipeRefresherLayout;
+    private ImageButton btnSearch, btnCloseSearchView, btnFavoriteNews;
+    private TextView tvQuery;
+    private EditText etSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvActualNews = findViewById(R.id.rvActualNews);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rvActualNews.setLayoutManager(llm);
+        swipeRefresherLayout = findViewById(R.id.swipeRefresherLayout);
+        swipeRefresherLayout.setColorSchemeResources(R.color.colorPrimaryDark);
+        btnSearch = findViewById(R.id.btnSearch);
+        btnCloseSearchView = findViewById(R.id.btnCloseSearchView);
+        btnFavoriteNews = findViewById(R.id.btnFavoriteNews);
+        tvQuery = findViewById(R.id.tvQuery);
+        etSearch = findViewById(R.id.etSearch);
     }
 }
